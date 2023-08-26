a3765b44c989d08dd972efe0665fc945d2d50181
Le Dev Du Coin <ledevducoin223@gmail.com>
2023-08-26 11:49:11 +0000

    Adding aws .ebextensions folder and django.config file

--- store/views/checkout.py
+++ store/views/checkout.py
@@ -28,8 +28,20 @@ def checkout(request):
 # Process to payement
 @login_required(login_url="login")
 def processOrder(request):
+    data = json(request.body)
+    print(data)
     transaction_id  = datetime.datetime.now().timestamp()
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+        first_name = data['shippingformData']['fist_name']
+        last_name = data['shippingformData']['last_name']
+
+        total = data['shippingformData']['total']
+        if total != order.get_cart_total:
+            return JsonResponse("Un probleme est survenu lors de la verification du montant total.")
+        else:
+            order.transaction_id = transaction_id
+            order.ip = request.session['REMOTE_ADDR']
     return JsonResponse("Payement effectue", safe=False)
