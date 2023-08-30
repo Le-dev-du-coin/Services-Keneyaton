a3dc32a79e04ee262b72faae063b8c394d889fd2
Le Dev Du Coin <ledevducoin223@gmail.com>
2023-08-30 10:01:52 +0000

    Change cart_context_processor file

--- store/cart_context_processor.py
+++ store/cart_context_processor.py
@@ -12,10 +12,12 @@ def cart_items(request):
     except:
         if 'device' in request.session:
             device = request.session['device']
-
-        customer, created = Customer.objects.get_or_create(device=device)
-        order, created = Order.objects.get_or_create(
+            customer, created = Customer.objects.get_or_create(device=device)
+            order, created = Order.objects.get_or_create(
                 customer=customer, complete=False
             )
-        cart_items = order.get_cart_items
+            cart_items = order.get_cart_items
+        else:
+            cart_items = 0
+        
     return dict(cart_items=cart_items)
